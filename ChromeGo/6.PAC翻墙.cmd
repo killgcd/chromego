32829640055fd6ca42b6f2b6c087f49ce36565f4
killgcd <linling07@gmail.com>
2020-01-07 09:48:01 +0800

    update

--- ChromeGo/6.PAC翻墙.cmd
+++ ChromeGo/6.PAC翻墙.cmd
@@ -4,5 +4,13 @@ cls
 IF EXIST %~dp0Browser\chrome.exe (
     start %~dp0Browser\chrome.exe --user-data-dir=%~dp0chrome-user-data --proxy-pac-url=https://raw.githubusercontent.com/bannedbook/fanqiang/master/jw/jwproxy.js https://www.bannedbook.org/bnews/fq/?utm_source=chromepac
 ) ELSE (
+	%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe" >nul 2>&1
+	IF  not errorlevel 1 (
     start chrome.exe --user-data-dir=%~dp0chrome-user-data --proxy-pac-url=https://raw.githubusercontent.com/bannedbook/fanqiang/master/jw/jwproxy.js https://www.bannedbook.org/bnews/fq/?utm_source=chromepac
+	) else ( 
+		echo Chrome浏览器不存在或没有正确安装，请尝试重新安装Chrome浏览器
+		echo 或者采用以下办法：
+		echo 右键点桌面的Google Chrome图标，再点属性，找到chrome.exe文件的路径，然后打开那个目录，把chrome.exe 连同那个目录下的所有子文件夹和文件，一起拷贝到ChromeGo文件夹下的Browser目录里面，然后重新启动ChromeGo的翻墙脚本。
+		pause
+	)
 )

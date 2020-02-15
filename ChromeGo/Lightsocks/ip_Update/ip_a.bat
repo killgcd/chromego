dcada522fff223fa8788028620e841f216d58f31
killgcd <linling07@gmail.com>
2020-02-15 20:23:16 +0800

    update

---
+++ ChromeGo/Lightsocks/ip_Update/ip_a.bat
@@ -0,0 +1,6 @@
+@Echo Off
+Title 从GitHub云端更新 Lightsocks 最新配置
+cd /d %~dp0
+..\..\wget -O configA.ini --ca-certificate=ca-bundle.crt -c https://raw.githubusercontent.com/Alvin9999/PAC/master/lightsocks/config.ini
+rem https://cdn.jsdelivr.net/gh/Alvin9999/PAC@latest/lightsocks/config.ini
+exit
--- ChromeGo/update.cmd
+++ ChromeGo/update.cmd
@@ -3,6 +3,6 @@ start /wait "" "%~dp0v2ray\ip_Update\ip_1.bat"
 start /wait "" "%~dp0Daze\ip_Update\ip_1.bat"
 start /wait "" "%~dp0SSR\ip_Update\ip_1.bat"
 start /wait "" "%~dp0Brook\ip_Update\ip_1.bat"
-start /wait "" "%~dp0Lightsocks\ip_Update\ip_1.bat"
+start /wait "" "%~dp0Lightsocks\ip_Update\ip_a.bat"
 start /wait "" "%~dp0trojan\ip_Update\ip_1.bat"
 pause
